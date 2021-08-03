f58f063582296366dcef63dc924377d4c8456fa1
jkh404 <56746942+jkh404@users.noreply.github.com>
2021-08-03 15:31:37 +0800

    增加对 netstandard2.0 的兼容

---
+++ SkyAutoPro/AutoProUtil.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SkyAutoPro
+{
+    internal static class AutoProUtil
+    {
+        public static TValue GetValueOrDefault<Tkey, TValue>(this Dictionary<Tkey, TValue> keyValues, Tkey key)
+        {
+            TValue value = default(TValue);
+            keyValues.TryGetValue(key, out value);
+            return value;
+        }
+    }
+}
